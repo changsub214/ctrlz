82347adcebc64bc8f0a332936e76d825940c64d3
Test <chanipm29@gmail.com>
2022-10-12 17:56:50 +0900

    ManyCommit

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,19 +2,41 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/views/Login'
 import SignUp from '@/views/SignUp'
+import Manual from '@/views/Manual'
+
+import First from '@/components/FirstView'
+import Second from '@/components/SecondView'
+
 
 Vue.use(VueRouter)
 
 const routes = [
     {
-        path: '/login',
-        name: 'Login',
-        component: Login
+        path: '/',
+        name: 'First',
+        component : First,
+        children:[
+            {
+                path: '/login',
+                name: 'Login',
+                component: Login
+            },
+            {
+                path:'/sign-up',
+                name:'SignUp',
+                component: SignUp
+            },
+            {
+                path:'/manual',
+                name:'Manual',
+                component: Manual
+            }
+        ]
     },
     {
-        path:'/sign-up',
-        name:'SignUp',
-        component: SignUp
+        path:'/sec',
+        name:'Second',
+        component : Second
     }
 ]
 
