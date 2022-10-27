63bcf00036886af5df8039fefdcaf6749c6cc2db
Test <chanipm29@gmail.com>
2022-10-27 12:25:41 +0900

    파일 삭제 추가 /  오류 수정 필요

--- frontend/src/components/SecondView.vue
+++ frontend/src/components/SecondView.vue
@@ -18,6 +18,10 @@
     :src="'/../'+item.filepath"
     width="600"></v-img>
     <h>{{item.filename}}</h>
+    <v-btn
+    icon>
+        <v-icon @click="deleteImage(item.filepath)">mdi-delete</v-icon>
+    </v-btn>
     <v-divider></v-divider>
     </div>
 </v-col>
@@ -61,6 +65,18 @@ export default {
                     this.images = res.data
                 }
             )
+            console.log(this.images.filepath,"images")
+        },
+        deleteImage(path){
+            console.log(path)
+            this.$axios.post('/files/deleteImage',{
+                email:this.$route.params.id,
+                filepath:path
+            })
+            .then(res=>{
+                console.log(res)
+                this.updateImage();
+            })
         }
     }
 }
