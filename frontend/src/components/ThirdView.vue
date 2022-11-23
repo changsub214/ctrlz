02e55425fc9fe9c523376f6d34c4a2539e2962f3
jaeduck1507 <ojd0708@naver.com>
2022-11-23 20:05:18 +0900

    fabric 기능구현

--- frontend/src/components/ThirdView.vue
+++ frontend/src/components/ThirdView.vue
@@ -1,136 +1,323 @@
 <template>
-    <v-app>
-      <v-app-bar
-        app
-        clipped-left
-        flat
-        height="72"
-      >App-Bar
-      <v-spacer></v-spacer>
-        <v-btn icon @click = "click_undo"><v-icon>mdi-undo</v-icon></v-btn>
-        <v-btn icon @click = "click_redo"><v-icon>mdi-redo</v-icon></v-btn>
-        <v-btn icon @click = "click_cut"><v-icon>mdi-content-cut</v-icon></v-btn>
-        <v-btn icon @click = "click_paste"><v-icon>mdi-content-paste</v-icon></v-btn>
-        <v-btn icon @click = "click_copy"><v-icon>mdi-content-copy</v-icon></v-btn>
-        <v-btn icon @click = "click_delete"><v-icon>mdi-delete</v-icon></v-btn>
-      </v-app-bar>
-  
-      <v-navigation-drawer
-        v-model="drawer"
-        app
-        clipped
-        permanent
-      >
-      <v-tabs
-      background-color="deep-purple accent-4"
-      dark
+  <v-app>
+    <v-app-bar
+      app
+      clipped-left
+      flat
+      height="72"
+    >App-Bar
+    <v-spacer></v-spacer>
+      <v-btn icon @click = "click_undo"><v-icon>mdi-undo</v-icon></v-btn>
+      <v-btn icon @click = "click_redo"><v-icon>mdi-redo</v-icon></v-btn>
+      <v-btn icon @click = "click_cut"><v-icon>mdi-content-cut</v-icon></v-btn>
+      <v-btn icon @click = "click_copy"><v-icon>mdi-content-copy</v-icon></v-btn>
+      <v-btn icon @click = "click_paste"><v-icon>mdi-content-paste</v-icon></v-btn>
+      <v-btn icon @click = "click_delete"><v-icon>mdi-delete</v-icon></v-btn>
+      <v-btn icon @click = "click_pen"><v-icon>mdi-pen</v-icon></v-btn>
+      <v-btn icon @click = "click_save"><v-icon>mdi-file-download</v-icon></v-btn>
+      <v-btn icon @click = "click_load"><v-icon>mdi-file-upload</v-icon></v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+      permanent
     >
-      <v-tab @click = "changeToPen">Pen</v-tab>
-      <v-tab @click = "changeToElec">Elec</v-tab>
-    </v-tabs>
-        <v-list>
-          <v-list-item
-            v-for="n in currentList"
-            :key="n"
-            @click="click_menu(n.action)"
-          >
-            <v-list-item-content>
-              <v-list-item-title> {{ n.name }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-  
-      <v-navigation-drawer
-        app
-        right
-      >
-        <v-list>
-          <v-list-item
-            v-for="n in 5"
-            :key="n"
-            link
-          >
-            <v-list-item-content>
-              <v-list-item-title>Item {{ n }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-  
-      <v-main>
-        <Drawing ref="childComponent" 
-    />
-      </v-main>
-    </v-app>
-  </template>
-  
-  <script>
+    <v-tabs
+    background-color="deep-purple accent-4"
+    dark
+  >
+    <v-tab @click = "changeToPen">Pen</v-tab>
+    <v-tab @click = "changeToElec">Elec</v-tab>
+  </v-tabs>
+      <v-list  v-if="listType === 'Pen'">
+        <v-list-item
+          v-for="n in PenList"
+          :key="n"
+          @click="click_menu(n.action)"
+        >
+          <v-list-item-content>
+            <v-list-item-title> {{ n.name }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <v-list v-if="listType ==='Elc'" class="overflow-y-auto">
+        <v-list-group 
+        v-for = "item in items" :key = "item.title" 
+        v-model = "item.active" :prepend-icon = "item.action"
+        no-action
+        >
+
+        <template v-slot:activator>
+          <v-list-item-content>
+            <v-list-item-title v-text = "item.title"></v-list-item-title>
+          </v-list-item-content>
+        </template>
+
+        <v-list-item
+          v-for = "child in item.items" :key = "child.image"
+          @click ="clickSymbol(child.image)">
+            <img :src = "child.image" width="100px" height="100px">
+        </v-list-item>
+      </v-list-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-navigation-drawer
+      app
+      right
+    >
+      <v-list>
+        <v-list-item
+          v-for="n in 5"
+          :key="n"
+          link
+        >
+          <v-list-item-content>
+            <v-list-item-title>Item {{ n }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <Drawing ref="childComponent" 
+  />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
 import Drawing from '../views/Drawing.vue';
 
-    export default {
-        components:{
-            Drawing,
-        },
-      data () {
-        return{
-            drawer: null,
-            PenList:[
-                {
-                    name:"Pen",
-                    action:"pen"
-                },
-                {
-                    name:"Cancel",
-                    action:"cancel"
-                },
-                {
-                    name:"Eraser",
-                    action:"erase"
-                }
-            ],
-            ElecList:["저항","전선","축전기","전원"],
-            currentList: ["Default"]
-        }
+  export default {
+      components:{
+          Drawing,
+      },
+    data () {
+      return{
+          drawer: null,
+          PenList:[
+              {
+                  name:"Pen",
+                  action:"pen"
+              },
+              {
+                  name:"Cancel",
+                  action:"cancel"
+              },
+              {
+                  name:"Eraser",
+                  action:"erase"
+              }
+          ],
+          items: [
+    {
+      action: "mdi-play",
+      title: "Wire",
     },
-        created(){
-            this.changeToPen()
-        },
-        methods:{
-            changeToPen(){
-                this.currentList = this.PenList
-            },
-            changeToElec(){
-                this.currentList = this.ElecList
-            },
-            click_menu(action){
-                if(action == "pen"){
-                    this.$refs.childComponent.pen_mode("red")
-                }
-                else if(action == "cancel"){
-                    this.$refs.childComponent.falseDrawing()
-                }
-            },
-
-            click_undo(){
-
-            },
-            click_redo(){
-
-            },
-            click_cut(){
-
-            },
-            click_copy(){
-
-            },
-            click_paste(){
-
-            },
-            click_delete(){
-
-            },
-
-        }
+    {
+      action: "mdi-play",
+      title: "Power Supply",
+      items: [
+        { image: require("@/assets/Electronic circuit symbols/Power_Supply/AC_Supply.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Power_Supply/BatteryStack.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Power_Supply/DC_Supply.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Power_Supply/Fuse.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Power_Supply/Fuse_IEC_.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Power_Supply/Fuse_IEEE_.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Power_Supply/Fuse_IEEE_2.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Power_Supply/Fuse_obsolete_.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Power_Supply/MonoCell_Battery.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Power_Supply/MutiCell_Battery_Tapped.svg") },
+      ],
+    },
+    {
+      action: "mdi-play",
+      title: "Transistors",
+      items: [
+        { image: require("@/assets/Electronic circuit symbols/transistors/BJT_NPN_.svg") },
+        { image: require("@/assets/Electronic circuit symbols/transistors/BJT_PNP_.svg") },
+        { image: require("@/assets/Electronic circuit symbols/transistors/Dual_Gate_MOSFET_N_.svg") },
+        { image: require("@/assets/Electronic circuit symbols/transistors/Dual_Gate_MOSFET_P_.svg") },
+        { image: require("@/assets/Electronic circuit symbols/transistors/MOSFET_N_.svg") },
+        { image: require("@/assets/Electronic circuit symbols/transistors/MOSFET_No_Bulk(N).svg") },
+        { image: require("@/assets/Electronic circuit symbols/transistors/MOSFET_No_Bulk(P).svg") },
+        { image: require("@/assets/Electronic circuit symbols/transistors/MOSFET(P).svg") },
+        { image: require("@/assets/Electronic circuit symbols/transistors/N_Channel_JFET.svg") },
+        { image: require("@/assets/Electronic circuit symbols/transistors/NMOS.svg") },
+        { image: require("@/assets/Electronic circuit symbols/transistors/P_Channel_JFET.svg") },
+        { image: require("@/assets/Electronic circuit symbols/transistors/PMOS.svg") },
+      ]
+    },
+    {
+      action: "mdi-play",
+      title: "Meters",
+      items: [
+        { image: require("@/assets/Electronic circuit symbols/Meters/AMMeter.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Meters/GalvanoMeter.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Meters/Oscilloscope.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Meters/VoltMeter.svg") },
+      ]
+    },
+    {
+      action: "mdi-play",
+      title: "Diodes",
+      items: [
+        { image: require("@/assets/Electronic circuit symbols/Diodes/Diode.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Diodes/LED_Diode.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Diodes/Photo_Diode.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Diodes/Schottky_Diode.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Diodes/Tunnel_Diode.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Diodes/Zener_Diode.svg") },
+      ]
+    },
+    {
+      action: "mdi-play",
+      title: "Capacitors",
+      items: [
+        { image: require("@/assets/Electronic circuit symbols/Capacitors/Capacitor_US__1_.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Capacitors/Capacitor_US__2_.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Capacitors/Electrolytic_Capacitor_US__1_.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Capacitors/Electrolytic_Capacitor_US__2_.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Capacitors/Trimmer_Variable_Capacitor_US__1_.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Capacitors/Trimmer_Variable_Capacitor_US__2_.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Capacitors/Tunning_Variable_Capacitor_US__1_.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Capacitors/Tunning_Variable_Capacitor_US__2_.svg") },
+      ]
+    },
+    {
+      action: "mdi-play",
+      title: "Resistors",
+      items: [
+        { image: require("@/assets/Electronic circuit symbols/Resistors/Potentiometer_Resistor(1).svg") },
+        { image: require("@/assets/Electronic circuit symbols/Resistors/Potentiometer_Resistor(2).svg") },
+        { image: require("@/assets/Electronic circuit symbols/Resistors/Resistor(1).svg") },
+        { image: require("@/assets/Electronic circuit symbols/Resistors/Resistor(2).svg") },
+        { image: require("@/assets/Electronic circuit symbols/Resistors/RHEOSTAT_Resistor(1).svg") },
+        { image: require("@/assets/Electronic circuit symbols/Resistors/RHEOSTAT_Resistor(2).svg") },
+        { image: require("@/assets/Electronic circuit symbols/Resistors/Trimmer_Resistor(1).svg") },
+        { image: require("@/assets/Electronic circuit symbols/Resistors/Trimmer_Resistor(2).svg") },          
+      ]
+    },
+    {
+      action: "mdi-play",
+      title: "Switches",
+      items: [
+        { image: require("@/assets/Electronic circuit symbols/Switches/Limit_Switch.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Switches/PushButton_NC.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Switches/PushButton_NO.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Switches/SPDT.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Switches/Switch_Disconnector.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Switches/Temperature_Switch.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Switches/Thermostat.svg") },
+      ]
+    },
+    {
+      action: "mdi-play",
+      title: "Output Device",
+      items: [
+        { image: require("@/assets/Electronic circuit symbols/Output_Deivices/Bell.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Output_Deivices/Buzzer.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Output_Deivices/Inductor.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Output_Deivices/Lamp.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Output_Deivices/Motor.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Output_Deivices/Piezo_Sounder.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Output_Deivices/Speaker.svg") },
+      ]
+    },
+    {
+      action: "mdi-play",
+      title: "Input Device",
+      items: [
+        { image: require("@/assets/Electronic circuit symbols/Input_Deivices/Electret_Microphone.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Input_Deivices/Microphone.svg") }
+      ]
+    },
+    {
+      action: "mdi-play",
+      title: "Miscellaneous",
+      items: [
+        { image: require("@/assets/Electronic circuit symbols/Miscellaneous/Antenae.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Miscellaneous/Dual_In-Line_IC.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Miscellaneous/Operational_AMP_1_.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Miscellaneous/Operational_AMP_2_.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Miscellaneous/Overcurrent_Relay_125-400A.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Miscellaneous/Quad_Flat_Package_IC.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Miscellaneous/Transformer_IronCore__1_.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Miscellaneous/Transformer_IronCore__2_.svg") },
+      ]
+    },
+    {
+      action: "mdi-play",
+      title: "Logic Gates",
+      items: [
+        { image: require("@/assets/Electronic circuit symbols/Logic_Gates/AND.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Logic_Gates/Buffer.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Logic_Gates/D-type_Filp_Flop.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Logic_Gates/D-type_RS_Filp_Flop.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Logic_Gates/Inverter.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Logic_Gates/NAND.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Logic_Gates/NOR.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Logic_Gates/OR.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Logic_Gates/XNOR.svg") },
+        { image: require("@/assets/Electronic circuit symbols/Logic_Gates/XOR.svg") },
+      ],
     }
-  </script>
+  ],
+          listType : "Pen"
+      }
+  },
+      created(){
+          this.changeToPen()
+      },
+      methods:{
+          changeToPen(){
+              this.listType = "Pen"
+          },
+          changeToElec(){
+              this.listType = "Elc"
+          },
+          click_menu(action){
+              if(action == "pen"){
+                  this.$refs.childComponent.pen_mode("red")
+              }
+              else if(action == "cancel"){
+                  this.$refs.childComponent.falseDrawing()
+              }
+          },
+          clickSymbol(image){
+            this.$refs.childComponent.drawSymbol(image)
+          },
+          click_undo(){
+            this.$refs.childComponent.undo()
+          },
+          click_redo(){
+            this.$refs.childComponent.redo()
+          },
+          click_cut(){
+            this.$refs.childComponent.cut()
+          },
+          click_copy(){
+            this.$refs.childComponent.copy()
+          },
+          click_paste(){
+            this.$refs.childComponent.paste()
+          },
+          click_delete(){
+            this.$refs.childComponent.delete()
+          },
+          click_pen(){
+            this.$refs.childComponent.linedrawing()
+          },
+          click_save(){
+            this.$refs.childComponent.linedrawing()
+          },
+          click_load(){
+            this.$refs.childComponent.linedrawing()
+          },
+
+      }
+  }
+</script>
--- frontend/src/views/Drawing.vue
+++ frontend/src/views/Drawing.vue
@@ -5,16 +5,25 @@
       v-bind:is="mode"
       @clickPen="pen_mode"/>
         <v-container>CANVAS
-        <canvas id="c" width="900" height="600"></canvas>
+        <canvas id="c" width="1200" height="800" style="border:1px solid #ccc"></canvas>
         <!--<v-btn @click = "mode_change">모드변경</v-btn>-->
         </v-container>
     </div>
   </template>
   
 
-<script>
+<script> 
+/* eslint-disable */
 import { fabric } from "fabric";
-let canvas;
+var canvas;
+var line;
+var isDown;
+var isLineDrawingMode = false;
+
+var isRedoing = false;
+var h = [];
+var _clipboard = false;
+
 export default {
     data(){
         return{
@@ -23,8 +32,143 @@ export default {
     },
     mounted() {
         canvas = new fabric.Canvas('c',{isDrawingMode: false})
+
+        canvas.on('object:added',function(){
+            if(!isRedoing){
+                h = [];
+            }
+            isRedoing = false;
+        })
+
+        document.onkeydown = this.onKeyDownHandler;
+        
     },
     methods:{
+        onKeyDownHandler(event){
+            var key;
+            if(window.event){
+                key = window.event.keyCode;
+            }
+            else{
+                key = event.keyCode;
+            }
+
+            switch(key){
+                case 8: //back space
+                    this.delete();
+                    break;
+                // Shortcuts
+                case 67: // Ctrl+C
+                        if(event.ctrlKey){
+                            event.preventDefault();
+                            this.copy();
+                        }
+                    break;
+                // Paste (Ctrl+V)
+                case 86: // Ctrl+V
+                        if(event.ctrlKey){
+                            event.preventDefault();
+                            this.paste();
+                        }
+                    break;
+                case 88: // Ctrl+X
+                        if(event.ctrlKey){
+                            event.preventDefault();
+                            this.cut();
+                        }
+                    break;
+                case 89: // Ctrl+Y
+                    if(event.ctrlKey){
+                        event.preventDefault();
+                        this.redo();
+                    }
+                    break;    
+                case 90: // Ctrl+Z
+                    if(event.ctrlKey){
+                        event.preventDefault();
+                        this.undo();
+                    }
+                    break;
+                default:
+                    // TODO
+                    break;
+            }
+        },
+        copy(){
+            canvas.getActiveObject().clone(function(cloned) {
+            _clipboard = cloned;
+            });
+        },
+        paste(){
+            _clipboard.clone(function(clonedObj) {
+            canvas.discardActiveObject();
+            clonedObj.set({
+                left: clonedObj.left + 10,
+                top: clonedObj.top + 10,
+                evented: true,
+            });
+            if (clonedObj.type === 'activeSelection') {
+                // active selection needs a reference to the canvas.
+                clonedObj.canvas = canvas;
+                clonedObj.forEachObject(function(obj) {
+                    canvas.add(obj);
+                });
+                // this should solve the unselectability
+                clonedObj.setCoords();
+            } else {
+                canvas.add(clonedObj);
+            }
+            _clipboard.top += 10;
+            _clipboard.left += 10;
+            canvas.setActiveObject(clonedObj);
+            canvas.requestRenderAll();
+            });
+        },
+        cut(){
+            if(canvas.getActiveObject() === null){
+                return;
+            }
+            canvas.getActiveObject().clone(function(cloned) {
+                _clipboard = cloned;
+                //remove after cloned to clipboard
+                if(canvas.getActiveObject().type == "activeSelection"){
+                    canvas.getActiveObject().getObjects().forEach(function(obj){
+                        canvas.remove(obj)
+                    })
+                }
+                else{
+                    canvas.remove(canvas.getActiveObject)
+                }
+            });
+        },
+        delete(){
+            var obj = canvas.getActiveObject()
+            if(obj.type == "activeSelection"){
+                obj.getObjects().forEach(function(o){
+                    canvas.remove(o)
+                })
+                console.log("remove")
+            }
+            else {
+                canvas.remove(obj)
+                console.log("remove1")
+            }
+
+            canvas.requestRenderAll()
+        },
+        redo(){
+  
+  if(h.length>0){
+    isRedoing = true;
+   canvas.add(h.pop());
+  }
+},
+        undo(){
+  if(canvas._objects.length>0){
+   h.push(canvas._objects.pop());
+   canvas.renderAll();
+  }
+},
         pen_mode(color){
             console.log("emit")
             canvas.isDrawingMode = true;
@@ -33,7 +177,58 @@ export default {
         },
         falseDrawing(){
             canvas.isDrawingMode = false;
+        },
+        
+        
+        drawSymbol(image){
+            console.log(image);
+            fabric.loadSVGFromURL(image, (objects) => {
+                var obj = fabric.util.groupSVGElements(objects);
+                obj.scale(0.2)
+                canvas.add(obj)
+            });
+            canvas.renderAll();
+        },
+        linedrawing(){
+            isLineDrawingMode = !isLineDrawingMode;
+            if(isLineDrawingMode){
+                canvas.selection=false;
+            canvas.on('mouse:down', function(o){
+                
+                isDown = true;
+                var pointer = canvas.getPointer(o.e);
+                var points = [ pointer.x, pointer.y, pointer.x, pointer.y ];
+                line = new fabric.Line(points, {
+                    strokeWidth: 1.5,
+                    fill: 'black',
+                    stroke: 'black',
+                    originX: 'center',
+                    originY: 'center'
+                });
+                canvas.add(line);
+            
+
+            });
+            canvas.on('mouse:move', function(o){
+                if (!isDown) return;
+                var pointer = canvas.getPointer(o.e);
+                line.set({ x2: pointer.x, y2: pointer.y });
+                canvas.renderAll();
+            });
+
+            canvas.on('mouse:up', function(o){
+                
+                isDown = false;
+            });
+            
+        }else{
+            canvas.selection=true;
+            canvas.off('mouse:down')
+            canvas.off('mouse:move')
+            canvas.off('mouse:up')
         }
+        },
+
     }
 
 }
